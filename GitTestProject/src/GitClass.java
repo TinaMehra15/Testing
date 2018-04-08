37f84226c8532fb07c972c45114ba96e48d1b8d0
tina.mehra <tina.mehra@holbmac0406>
2018-04-08 21:57:27 +0100

    Second change

--- GitTestProject/src/GitClass.java
+++ GitTestProject/src/GitClass.java
@@ -4,6 +4,7 @@ public class GitClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("First Commit");
+System.out.println("second commit");
 	}
 
 }
